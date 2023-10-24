dc4e12e6f9ef30eafddd2927f72861564a71b60e
Samantha Siqueira <samantharsiqueira@gmail.com>
2023-10-24 18:34:24 +0100

    primeiros exercicios

---
+++ fundamentos/3-introducao-javascript/2-array-for/desafiofatorial.js
@@ -0,0 +1,11 @@
+/* O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero. Por exemplo:
+
+O fatorial é representado pelo sinal !
+Exemplo de 4 fatorial:
+4! = 4 x 3 x 2 x 1 = 24
+
+Com base nessas informações, crie um algoritmo que imprima na tela o fatorial de 10. */
+
+let number = 1;
+ 
+for (index = 0 )
---
+++ fundamentos/3-introducao-javascript/2-array-for/inverterpalavra.js
@@ -0,0 +1,5 @@
+/* Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra.
+
+De olho na dica 👀: Pesquise sobre os seguintes métodos: split(), reverse() e join();*/
+
+let word = 'tryber';
