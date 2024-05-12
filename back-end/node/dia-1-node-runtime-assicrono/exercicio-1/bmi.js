639222804a868fa94e9deaf938e9fab9a6d8466e
Samantha Siqueira <samantharsiqueira@gmail.com>
2024-05-12 13:04:27 +0100

    bmi calculator

---
+++ back-end/node/dia-1-node-runtime-assicrono/exercicio-1/bmi.js
@@ -0,0 +1,22 @@
+const weightInKg = 83; 
+const heightInCm = 167; 
+
+function handleBMI(weight, height) {
+  console.log(`Weight: ${weight}, Height: ${height}`);
+
+  const heightInMeters = height / 100;
+  const heightSquared = heightInMeters ** 2;
+
+  const bmi = weight / heightSquared;
+
+  return bmi;
+}
+
+// A função main é o ponto de partida do nosso programa
+function main() {
+  const bmi = handleBMI(weightInKg, heightInCm);
+
+  console.log(`BMI: ${bmi.toFixed(2)}`);
+}
+
+main();
